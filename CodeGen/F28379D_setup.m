d209f19f330f2c199459cb18ece59ee8efc1a8e7
Manuel Barrios <mabf@energy.aau.dk>
2024-07-22 21:20:05 +0200

    I added a comment so you can identify where to change de Dead time.

--- CodeGen/F28379D_setup.m
+++ CodeGen/F28379D_setup.m
@@ -1,4 +1,4 @@
-%% Template prepared by Manuel Barrios April 8, 2024
+%% Template prepared by Manuel Barrios. Creation: April 8, 2024
 
 CPU_SYSCLKOUT           =   192.5E6;  %CPU Clock
 CPU_EPWMCLKDIV          =   1/2;    %By default
@@ -13,5 +13,5 @@ CPU_DAC_offset          =   CPU_DAC_bits/2;
 CPU_ADC_inputvoltage    =   3;      
 CPU_ADC_res             =   3/CPU_ADC_bits;
 CPU_CLK_CYCLE           =   CPU_SYSCLKOUT*CPU_EPWMCLKDIV;
-CPU_DT_HF               =   250E-9;
+CPU_DT_HF               =   250E-9;     %Modify this value (nano seconds) in case you want to change the Deadtime.
 CPU_RF_HF               =   cast(CPU_DT_HF/CPU_CLK_CYCLE^-1,"int16");  %RF means RED and FED
